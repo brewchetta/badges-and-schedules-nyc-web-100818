b0f2894624632a039c1ed848643be46857ce3475
Chett Tiller <chett.tiller@gmail.com>
2018-09-22 11:49:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,7 +26,7 @@ def printer(names)
     puts batch_badge_creator(names)
     puts assign_rooms(names)
   end
-  if names.type == Array
+  if names.class == Array
     names.each do |name|
       puts batch_badge_creator(name)
       puts assign_rooms(name)
